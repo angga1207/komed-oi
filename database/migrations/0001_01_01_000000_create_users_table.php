3f1f5f93a6bf8ebb9b05686aac47003e80f0f4af
angga1207 <63151469+angga1207@users.noreply.github.com>
2025-01-31 14:24:19 +0700

    first

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -11,13 +11,141 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('roles', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('slug');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+
+        Schema::create('users', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('fullname');
+            $table->string('first_name')->nullable();
+            $table->string('last_name')->nullable();
+            $table->string('username')->unique();
+            $table->longText('email')->unique();
+            $table->string('photo')->default('default.png');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('google_id')->nullable();
+            $table->enum('status', ['pending', 'active', 'suspend', 'banned'])->default('pending')->nullable();
+            $table->string('role_id');
             $table->rememberToken();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+
+        Schema::create('pers_profile', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->string('nik')->unique();
+            $table->text('nama_perusahaan')->nullable();
+            $table->text('nama_media')->nullable();
+            $table->text('alias')->nullable();
+            $table->text('jenis_media')->nullable();
+            $table->longText('alamat_media')->nullable();
+            $table->longText('logo_media')->nullable();
+
+            $table->longText('whatsapp')->nullable();
+            $table->longText('email')->nullable();
+            $table->longText('no_npwp')->nullable();
+            $table->longText('no_ref_bank')->nullable();
+            $table->longText('no_giro_perusahaan')->nullable();
+            $table->longText('website')->nullable();
+            $table->text('file_ktp')->nullable()->after('whatsapp');
+            $table->longText('profil_perusahaan')->nullable();
+
+            $table->timestamp('verification_deadline')->nullable();
+
+
+            $table->enum('verified_status', ['pending', 'verified', 'suspend', 'banned'])->default('pending')->nullable();
+            $table->enum('tier', ['0', '1', '2', '3'])->nullable();
+            $table->double('tier_point')->default(0);
+
+            $table->text('cakupan_media')->nullable();
+            $table->text('jumlah_oplah')->nullable();
+            $table->text('sebaran_oplah')->nullable();
+            $table->text('status_wartawan')->nullable();
+            $table->text('kompetensi_wartawan')->nullable();
+            $table->text('status_dewan_pers')->nullable();
+            $table->text('kantor')->nullable();
+            $table->text('frekuensi_terbitan')->nullable();
+            $table->text('terbitan_3_edisi_terakhir')->nullable();
+
+            $table->text('file_jumlah_oplah')->nullable();
+            $table->text('file_status_wartawan')->nullable();
+            $table->text('file_kompetensi_wartawan')->nullable();
+            $table->text('file_status_dewan_pers')->nullable();
+            $table->text('file_terbitan_3_edisi_terakhir')->nullable();
+
+            $table->longText('extend_verification_message')->nullable()->after('verification_deadline');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+
+
+        // $detailMediaCetak = [
+        //     'cakupan_media' => null,
+        //     'jumlah_oplah' => null,
+        //     'sebaran_oplah' => null,
+        //     'status_wartawan' => null,
+        //     'kompetensi_wartawan' => null,
+        //     'status_dewan_pers' => null,
+        //     'kantor' => null,
+        //     'frekuensi_terbitan' => null,
+        //     'terbitan_3_edisi_terakhir' => null,
+
+        //     'file_jumlah_oplah' => null,
+        //     'file_status_wartawan' => null,
+        //     'file_kompetensi_wartawan' => null,
+        //     'file_status_dewan_pers' => null,
+        //     'file_terbitan_3_edisi_terakhir' => null,
+        // ];
+
+
+        // => FOR PERS PROFILE FILES
+        // $berkas = [
+        //     'akta_pendirian' => null,
+        //     'sk_kemenkumham' => null,
+        //     'izin_ipp' => null,
+        //     'izin_isr' => null,
+        //     'siup' => null,
+        //     'tdp_penyiaran_60102' => null,
+        //     'tdp_penerbitan_63122' => null,
+        //     'tdp_penerbitan_58130' => null,
+        //     'npwp' => null,
+        //     'spt_terakhir' => null,
+        //     'sp_cakupan_wilayah' => null,
+        //     'sp_pimpinan' => null,
+        //     'sk_biro_iklan' => null,
+        //     'situ' => null,
+        //     'sk_domisili' => null,
+        //     'surat_tugas_wartawan' => null,
+        // ];
+
+        Schema::create('pers_profile_files', function (Blueprint $table) {
+            $table->id();
+            $table->integer('pers_profile_id');
+            $table->string('file_name');
+            $table->string('file_path');
+            $table->string('file_type');
+            $table->timestamps();
+        });
+
+        Schema::create('laporan_pers', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('media_pers_id')->nullable();
+            $table->string('judul')->nullable();
+            $table->string('slug')->nullable();
+            $table->dateTime('tanggal_publikasi')->nullable();
+            $table->string('link')->nullable();
+            $table->json('lampiran')->nullable();
+            $table->longText('catatan_admin')->nullable();
+            $table->longText('catatan_pelapor')->nullable();
+            $table->enum('status', ['pending', 'verified', 'rejected', 'paid', 'unpaid'])->default('pending');
+            $table->softDeletes();
             $table->timestamps();
         });
 
@@ -42,7 +170,11 @@ return new class extends Migration
      */
     public function down(): void
     {
+        Schema::dropIfExists('roles');
         Schema::dropIfExists('users');
+        Schema::dropIfExists('pers_profile');
+        Schema::dropIfExists('pers_profile_files');
+        Schema::dropIfExists('laporan_pers');
         Schema::dropIfExists('password_reset_tokens');
         Schema::dropIfExists('sessions');
     }
---
+++ database/seeders/BaseSeeder.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class BaseSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $roles = [
+            [
+                'name' => 'Developer',
+                'slug' => 'developer'
+            ],
+            [
+                'name' => 'Admin',
+                'slug' => 'admin'
+            ],
+            [
+                'name' => 'Verifikator',
+                'slug' => 'verifikator'
+            ],
+            [
+                'name' => 'User',
+                'slug' => 'user'
+            ]
+        ];
+
+        foreach ($roles as $role) {
+            $checkRoles = DB::table('roles')->where('slug', $role['slug'])->first();
+            if (!$checkRoles) {
+                DB::table('roles')->insert($role);
+            } else {
+                DB::table('roles')->where('slug', $role['slug'])->update($role);
+            }
+        }
+
+        $users = [
+            [
+                'fullname' => 'Developer',
+                'first_name' => 'Developer',
+                'last_name' => '',
+                'username' => 'developer',
+                'email' => 'developer@komed.oganilirkab.go.id',
+                'photo' => '/storage/images/users/default.png',
+                'email_verified_at' => now(),
+                'password' => bcrypt('jens5522'),
+                'status' => 'active',
+                'role_id' => 1,
+            ],
+            [
+                'fullname' => 'Admin',
+                'first_name' => 'Admin',
+                'last_name' => '',
+                'username' => 'admin',
+                'email' => 'admin@komed.oganilirkab.go.id',
+                'photo' => '/storage/images/users/default.png',
+                'email_verified_at' => now(),
+                'password' => bcrypt('password'),
+                'status' => 'active',
+                'role_id' => 2,
+            ],
+            [
+                'fullname' => 'Verifikator',
+                'first_name' => 'Verifikator',
+                'last_name' => '',
+                'username' => 'verifikator',
+                'email' => 'verifikator@komed.oganilirkab.go.id',
+                'photo' => '/storage/images/users/default.png',
+                'email_verified_at' => now(),
+                'password' => bcrypt('password'),
+                'status' => 'active',
+                'role_id' => 3,
+            ],
+            [
+                'fullname' => 'User',
+                'first_name' => 'User',
+                'last_name' => '',
+                'username' => 'user',
+                'email' => 'user@komed.oganilirkab.go.id',
+                'photo' => '/storage/images/users/default.png',
+                'email_verified_at' => now(),
+                'password' => bcrypt('password'),
+                'status' => 'active',
+                'role_id' => 4,
+            ]
+        ];
+
+        foreach ($users as $user) {
+            $checkUsers = DB::table('users')->where('username', $user['username'])->first();
+            if (!$checkUsers) {
+                DB::table('users')->insert($user);
+            } else {
+                DB::table('users')->where('username', $user['username'])->update($user);
+            }
+        }
+    }
+}
