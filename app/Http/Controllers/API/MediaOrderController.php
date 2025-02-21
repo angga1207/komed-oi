7c2e6f4050ada6a2456018227b297c8cdb92d047
angga1207 <63151469+angga1207@users.noreply.github.com>
2025-02-21 11:12:55 +0700

    Update MediaOrderController.php

--- app/Http/Controllers/API/MediaOrderController.php
+++ app/Http/Controllers/API/MediaOrderController.php
@@ -331,7 +331,7 @@ class MediaOrderController extends Controller
         if (!$data) {
             return $this->notFoundResponse('Media Order tidak dapat kami temukan');
         }
-        if ($data->status != 'sent') {
+        if (in_array($data->status, ['sent', 'rejected'])) {
             return $this->validationErrorResponse('Media Order tidak dalam status yang benar');
         }
         $agenda = DB::table('agendas')->where('id', $data->agenda_id)->first();
